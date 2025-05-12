6aca27988a0baf679b66c4bafab7acae1eebe34f
JuanCali999 <u202121935@upc.edu.pe>
2025-05-12 14:59:43 -0500

    fix: implemented redirect pacient

--- components/PacientesLista/ListarPacientes.tsx
+++ components/PacientesLista/ListarPacientes.tsx
@@ -189,15 +189,14 @@ export default function ListarPacientes() {
           {filteredPacientes.map((paciente) => (
             <tr
               key={paciente.idPaciente}
-              className="bg-background border-b hover:bg-gray-100 cursor-pointer"
-              onClick={() => redirectToPaciente(paciente.idPaciente)}
+              className="bg-background border-b hover:bg-gray-100 cursor-pointer"              
             >
-              <td className="px-4 py-2 text-2xl rounded-l-[34px]">o</td>
-              <td className="px-2 py-2">{paciente.nombre}</td>
-              <td className="px-2 py-2">{paciente.codigo}</td>
-              <td className="px-2 py-2">{paciente.DNI}</td>
-              <td className="px-2 py-2">{paciente.email}</td>
-              <td className="py-2">{paciente.celular}</td>
+              <td className="px-4 py-2 text-2xl rounded-l-[34px]" onClick={() => redirectToPaciente(paciente.idPaciente)}>o</td>
+              <td className="px-2 py-2" onClick={() => redirectToPaciente(paciente.idPaciente)}>{paciente.nombre}</td>
+              <td className="px-2 py-2" onClick={() => redirectToPaciente(paciente.idPaciente)}>{paciente.codigo}</td>
+              <td className="px-2 py-2" onClick={() => redirectToPaciente(paciente.idPaciente)}>{paciente.DNI}</td>
+              <td className="px-2 py-2" onClick={() => redirectToPaciente(paciente.idPaciente)}>{paciente.email}</td>
+              <td className="py-2" onClick={() => redirectToPaciente(paciente.idPaciente)}>{paciente.celular}</td>
               <td className="py-2 rounded-r-[34px]">
                 <div className="w-full h-full flex items-center justify-center">
                   <div className="flex flex-row items-center justify-center gap-x-4">
