2a3751e0801b7ff867f772b372d05b61f29077bc
GiovanniAlca <gra483120@gmail.com>
2025-06-20 17:55:43 -0500

    Añadiendo vista de marketing/plantillas/detalles de campaña

---
+++ app/user/marketing/detalle/page.tsx
@@ -0,0 +1,10 @@
+// app/marketing/detalle/page.tsx
+"use client";
+import React from "react";
+import DetalleCampania from "@/components/User/Marketing/DetallePlantilla/detalle";
+
+const DetalleCampaniaPage = () => {
+  return <DetalleCampania />;
+};
+
+export default DetalleCampaniaPage;
