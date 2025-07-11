8793d2f74c0759a3130e1edcb23dd756e614cee4
cat_ta4 <hugoalva291004@gmail.com>
2025-07-11 11:00:57 -0500

    Imagenes de servicio recortadas

--- app/user/personal/page.tsx
+++ app/user/personal/page.tsx
@@ -2,10 +2,7 @@ import PersonalComponent from "@/components/CrearUsuario/PersonalComponent";
 
 export default function Personal() {
   return(
-    <div className="flex justify-center items-center h-screen"
-    style={{
-      backgroundImage: `linear-gradient(to right, #3616D87A 0%, #3616D870 30%, #7863E37A 70%, #7863E370 100%)`,
-     }}
+    <div className="flex justify-center items-center h-screen bg-large-gradient"
     >
     <PersonalComponent />    
   </div>
