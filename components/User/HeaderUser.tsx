b79e53d48583b59e01937bbb4df444f6af35d06c
Aaron-Ccente <aronccente@gmail.com>
2025-07-25 17:57:46 -0500

    Agregado icono para regresar con redireccionamiento segun las rutas visitadas

--- components/User/HeaderUser.tsx
+++ components/User/HeaderUser.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import CerrarSesion from "../CerrarSesion";
+import Link from "next/link";
+import { Icons } from "@/icons";
 
 interface Props {
     title: string;
@@ -8,10 +10,28 @@ interface Props {
 const HeaderUser = ({title}: Props) => {
   return (
     <header className="pt-10 pb-6 z-30 px-10">
+       
       <div className="flex items-start justify-between ">
+        <div className="flex items-center">
+          <Link 
+          href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                window.history.back();
+              }}>
+          <span
+            dangerouslySetInnerHTML={{
+              __html: Icons.arrow.replace(
+                /<svg /,
+                '<svg fill="hsl(var(--primary))" transform="rotate(90)" width="2em" height="2em" stroke="hsl(var(--primary))" stroke-width="2" '
+              ),
+            }}
+          />
+          </Link>
         <h1 className="text-2xl md:text-4xl font-bold text-[#634be3]">
           {title}
         </h1>
+        </div>
         <div className="flex gap-x-5 ">
           <CerrarSesion />
         </div>
