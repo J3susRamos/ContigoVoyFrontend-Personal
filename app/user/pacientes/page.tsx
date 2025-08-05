85c34c111915059469d3dfcdc1566799ae84c30a
EstefanoK <estefanotesen07@gmail.com>
2025-08-05 17:51:43 -0500

    modificando colores modo oscuro

--- app/user/pacientes/page.tsx
+++ app/user/pacientes/page.tsx
@@ -35,7 +35,8 @@ export default function Pacientes() {
     }
 
     return (
-        <div className="bg-[#f6f7f7] dark:bg-background min-h-screen flex flex-col">
+       <div className="bg-[#f6f7f7] dark:bg-[#121212] dark:text-[#f6f7f7] min-h-screen flex flex-col">
+
             <HeaderUser title="Lista de pacientes"/>
             <NavbarPacientes
                 filterValue={filterValue}
