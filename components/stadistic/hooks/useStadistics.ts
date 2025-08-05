c8474c4b9528b8e0fc1d2ed426a4932a2f7538f8
Lord <toledosandro59@gmail.com>
2025-08-05 13:26:59 -0500

    Corrigiendo getButtonClasses

--- components/stadistic/hooks/useStadistics.ts
+++ components/stadistic/hooks/useStadistics.ts
@@ -26,7 +26,7 @@ export default function useStadistics() {
   };
 
   const getButtonClasses = (isActive: boolean) => {
-    const baseClasses = "w-full md:w-auto inline-flex items-center justify-center gap-2 whitespace-nowrap shadow h-10 md:h-11 text-sm md:text-base font-semibold rounded-full px-4 md:px-8 py-2 transition-all duration-200 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-offset-2";
+    const baseClasses = "w-full md:w-auto inline-flex items-center justify-center gap-2 whitespace-nowrap shadow h-10 md:h-11 text-sm md:text-base font-semibold rounded-full px-4 md:px-8 py-2 transition-all duration-200 ease-in-out transform hover:scale-105";
 
     return isActive
       ? `${baseClasses} bg-white dark:bg-background text-primary dark:text-primary-foreground shadow-lg border-2 border-primary/20`
