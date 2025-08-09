1eedb86d3875758795b6a7673f0f0430383917e0
cat_ta4 <hugoalva291004@gmail.com>
2025-08-09 09:26:17 -0500

    Reponiendo iconos svg en el footer

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,5 +1,6 @@
 "use client";
 import RedesSociales from "@/components/footer/RedesSociales";
+import { Image } from "@heroui/react";
 import { motion } from "framer-motion";
 import Link from "next/link";
 import { ReactSVG } from "react-svg";
@@ -7,33 +8,64 @@ import { ReactSVG } from "react-svg";
 export default function Footer() {
   const leftLinks = [
     { text: "Inicio", href: "/", title: "Volver a la página principal" },
-    { text: "Sobre Nosotros", href: "/sobreNosotros", title: "Conoce nuestro equipo y misión" },
-    { text: "Servicios", href: "/servicios/terapia/infantes", title: "Terapia especializada para niños, adolescentes, adultos y más." },
-    { text: "Reservar cita", href: "/ReservarCita", title: "Agenda una consulta en línea" },
+    {
+      text: "Sobre Nosotros",
+      href: "/sobreNosotros",
+      title: "Conoce nuestro equipo y misión",
+    },
+    {
+      text: "Servicios",
+      href: "/servicios/terapia/infantes",
+      title: "Terapia especializada para niños, adolescentes, adultos y más.",
+    },
+    {
+      text: "Reservar cita",
+      href: "/ReservarCita",
+      title: "Agenda una consulta en línea",
+    },
   ];
 
   const rightLinks = [
-    { text: "Contáctanos", href: "/contactanos", title: "WhatsApp, email y redes sociales" },
-    { text: "Preguntas Frecuentes", href: "/PreguntasFrecuentes", title: "Respuestas a dudas comunes" },
-    { text: "Blog", href: "/blog", title: "Artículos sobre salud mental y consejos" },
-    { text: "Iniciar sesión", href: "/login", title: "Acceso para profesionales y administradores." },
+    {
+      text: "Contáctanos",
+      href: "/contactanos",
+      title: "WhatsApp, email y redes sociales",
+    },
+    {
+      text: "Preguntas Frecuentes",
+      href: "/PreguntasFrecuentes",
+      title: "Respuestas a dudas comunes",
+    },
+    {
+      text: "Blog",
+      href: "/blog",
+      title: "Artículos sobre salud mental y consejos",
+    },
+    {
+      text: "Iniciar sesión",
+      href: "/login",
+      title: "Acceso para profesionales y administradores.",
+    },
   ];
 
   return (
-    <footer className="h-auto bg-[#634AE2] dark:bg-gray-900 dark:text-white text-white isolate">
-
+    <footer className="h-auto bg-[#634AE2] dark:bg-gray-900 text-white isolate ">
       <div className="lg:max-w-6xl p-10 md:px-11 mx-auto">
         {/* Contenido Principal */}
         <div className="flex flex-col md:flex-row justify-between items-start space-y-8 md:space-y-0">
           {/* Logo centrado con redes */}
-          <div className="flex flex-col shrink-2 justify-center w-full md:w-[220px]">
+          <div className="flex flex-col shrink-2  justify-center  w-full md:w-[220px] ">
             <motion.div
               initial={{ opacity: 0, scale: 0.8 }}
               animate={{ opacity: 1, scale: 1 }}
               transition={{ delay: 0.6 }}
               className="flex-shrink-0"
             >
-
+              <ReactSVG
+                src="/logofooter.svg"
+                title="Contigo Voy - Terapia Psicológica"
+                className="w-[150px] h-[60px]"
+              />
             </motion.div>
 
             {/* Redes sociales debajo del logo */}
@@ -44,20 +76,22 @@ export default function Footer() {
 
           {/* Enlaces */}
           <div className="flex flex-col text-start w-full md:w-auto">
-            <h3 className="text-2xl font-semibold mb-4 dark:text-white">Enlaces</h3>
+            <h3 className="text-2xl font-semibold mb-4 text-white">Enlaces</h3>
+
             <div className="grid grid-cols-1 sm:grid-cols-2 gap-x-2 gap-y-2 sm:gap-y-4">
               {/* Columna izquierda */}
-              <ul className="space-y-2 sm:space-y-3 text-xs dark:text-white">
+              <ul className="space-y-2 sm:space-y-3 text-xs text-white">
                 {leftLinks.map((link) => (
                   <li key={link.text}>
-                    <Link href={link.href} passHref legacyBehavior>
-                      <a
-                        title={link.title}
-                        className="relative group dark:text-white transition-colors duration-300 cursor-pointer"
-                      >
+                    <Link
+                      href={link.href}
+                      passHref
+                      legacyBehavior
+                      className="text-purple-100 hover:text-white transition-colors duration-500 flex items-center group relative"
+                    >
+                      <a title={link.title} className="relative group">
                         {link.text}
-                        {/* Línea debajo del texto */}
-                        <span className="absolute bottom-0 left-0 w-0 transition-all h-0.5 bg-white group-hover:w-full dark:bg-white group-hover:dark:bg-white inline-block duration-500"></span>
+                        <span className="absolute bottom-0 left-0 w-0 transition-all h-0.5 bg-white group-hover:w-full inline-block duration-500"></span>
                       </a>
                     </Link>
                   </li>
@@ -65,17 +99,18 @@ export default function Footer() {
               </ul>
 
               {/* Columna derecha */}
-              <ul className="space-y-2 sm:space-y-3 text-xs dark:text-white">
+              <ul className="space-y-2 sm:space-y-3 text-xs text-white">
                 {rightLinks.map((link) => (
                   <li key={link.text}>
-                    <Link href={link.href} passHref legacyBehavior>
-                      <a
-                        title={link.title}
-                        className="relative group dark:text-white transition-colors duration-300  cursor-pointer"
-                      >
+                    <Link
+                      href={link.href}
+                      passHref
+                      legacyBehavior
+                      className="text-purple-100 hover:text-white transition-colors duration-500 flex items-center group relative"
+                    >
+                      <a title={link.title} className="relative group">
                         {link.text}
-                        {/* Línea debajo del texto */}
-                        <span className="absolute bottom-0 left-0 w-0 transition-all h-0.5 bg-white group-hover:w-full dark:bg-white group-hover:dark:bg-white inline-block duration-300"></span>
+                        <span className="absolute bottom-0 left-0 w-0 transition-all h-0.5 bg-white group-hover:w-full inline-block duration-500"></span>
                       </a>
                     </Link>
                   </li>
@@ -87,17 +122,20 @@ export default function Footer() {
           {/* Redes sociales y contacto */}
           <div className="flex flex-col space-y-6">
             <div className="flex flex-col text-start w-full md:w-auto">
-              <h3 className="text-2xl font-semibold mb-4 dark:text-white">Contacto</h3>
+              <h3 className="text-2xl font-semibold mb-4 text-white">
+                Contacto
+              </h3>
+
               <div className="flex flex-col space-y-3 items-start">
                 {/* Perú */}
-                <div className="flex flex-col sm:flex-row sm:items-center sm:gap-2 text-start w-full">
+                <div className="flex flex-col sm:flex-row  sm:items-center sm:gap-2 text-start w-full">
                   <a
                     href="https://w.app/dvwynv"
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="flex items-center space-x-1 border border-white dark:border-white rounded-full px-2 py-0.5 hover:bg-green-600/80 transition-colors w-fit"
+                    className="group flex items-center space-x-1 border border-white hover:border-[#634AE2] rounded-full px-2 py-0.5 hover:bg-gray-100/80 transition-colors w-fit"
                   >
-                    <span className="text-white duration-250  text-[10px]">
+                    <span className="text-white  group-hover:text-[#634AE2] duration-250  text-[10px]">
                       Perú
                     </span>
                     <ReactSVG
@@ -113,20 +151,20 @@ export default function Footer() {
                       }}
                     />
                   </a>
-                  <span className="text-[12px] mt-1 sm:mt-0 dark:text-white">
+                  <span className="text-[12px] mt-1 sm:mt-0">
                     +51 983 027 828
                   </span>
                 </div>
 
                 {/* Argentina */}
-                <div className="flex flex-col sm:flex-row sm:items-center sm:gap-2 text-start">
+                <div className="flex flex-col sm:flex-row  sm:items-center sm:gap-2 text-start">
                   <a
                     href="https://w.app/qv4uqn"
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="flex items-center space-x-1 border border-white dark:border-white rounded-full px-2 py-0.5 hover:bg-green-600/80 transition-colors w-fit"
+                    className="group flex items-center space-x-1 border border-white hover:border-[#634AE2] rounded-full px-2 py-0.5 hover:bg-gray-100/80 transition-colors w-fit"
                   >
-                    <span className="text-white duration-250  text-[10px]">
+                    <span className="text-white  group-hover:text-[#634AE2] duration-250  text-[10px]">
                       Argentina
                     </span>
                     <ReactSVG
@@ -142,7 +180,7 @@ export default function Footer() {
                       }}
                     />
                   </a>
-                  <span className="text-[12px] mt-1 sm:mt-0 dark:text-white">
+                  <span className="text-[12px] mt-1 sm:mt-0">
                     +54 922 130 326 75
                   </span>
                 </div>
