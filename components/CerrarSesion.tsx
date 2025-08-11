3898467b4c992c39bcee338f56e24e577a3063b8
Aaron-Ccente <aronccente@gmail.com>
2025-08-11 17:08:07 -0500

    Componente Editar Perfil solo se visualiza cuando el usuario es PSICOLOGO

--- components/CerrarSesion.tsx
+++ components/CerrarSesion.tsx
@@ -67,6 +67,7 @@ export default function CerrarSesion() {
           <path d="M480-481q-66 0-108-42t-42-108q0-66 42-108t108-42q66 0 108 42t42 108q0 66-42 108t-108 42ZM160-220v-34q0-38 19-65t49-41q67-30 128.5-45T480-420q62 0 123 15.5T731-360q31 14 50 41t19 65v34q0 25-17.5 42.5T740-160H220q-25 0-42.5-17.5T160-220Z" />
         </svg>
       )}
+      {user?.rol === 'PSICOLOGO'?
       <div
         className={`relative`}
         onMouseEnter={() => setMenuOpen(true)}
@@ -108,7 +109,14 @@ export default function CerrarSesion() {
             </div>
           </div>
         )}
-      </div>
+      </div>:
+      <Button
+          className="text-white px-4 py-2 text-center text-base flex justify-start  bg-[#634AE2] hover:bg-[#4b36b3] hover:text-red-600 transition-colors rounded-t-xl"
+          onPress={logout}
+          >
+          <LogOutIcon/>
+          <p className="text-white">Cerrar sesión</p>
+      </Button>}
       <Editar
         isEditOpen={isEditOpen}
         setIsEditOpen={setIsEditOpen}
