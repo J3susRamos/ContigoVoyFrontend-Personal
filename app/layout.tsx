308cbc0e1467732b1300a6816e52464846592cd3
Lord <toledosandro59@gmail.com>
2025-09-01 11:06:23 -0500

    Usando Script de Next
    Signed-off-by: Lord <toledosandro59@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { Lexend } from "next/font/google";
 import { ToastContainer } from "react-toastify";
 import React from "react";
+import Script from "next/script";
 import ClientLayoutWrapper from "@/components/ClientLayoutWrapper";
 
 const lexend = Lexend({ subsets: ["latin"] });
@@ -14,34 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        {/* Google tag (gtag.js) */}
-        <script 
-          async 
-          src="https://www.googletagmanager.com/gtag/js?id=G-VTMM4ZV0VP"
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-VTMM4ZV0VP');
-            `,
-          }}
-        />
-        {/* Google Tag Manager */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-MVTPCQFR');`,
-          }}
-        />
-        {/* End Google Tag Manager */}
-      </head>
+      <head></head>
       <body className={`${lexend.className} antialiased`}>
         {/* Google Tag Manager (noscript) */}
         <noscript>
@@ -53,7 +27,30 @@ export default function RootLayout({
           ></iframe>
         </noscript>
         {/* End Google Tag Manager (noscript) */}
-        
+
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-VTMM4ZV0VP"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-VTMM4ZV0VP');
+          `}
+        </Script>
+
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MVTPCQFR');`}
+        </Script>
+
         <ThemeProvider
           attribute="class"
           defaultTheme="light"
